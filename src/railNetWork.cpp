a5b69381acbe85fca34ce5b245e0b78789de9f21
Abuduwahapu Reheman <areheman@k2r4p9.42mulhouse.fr>
2025-02-26 13:36:02 +0100

    ok2

---
+++ src/railNetWork.cpp
@@ -0,0 +1,16 @@
+#include "railNetWork.hpp"
+
+RailNetWork::RailNetWork(const RailNetWork& obj){
+    *this = obj;
+}
+
+RailNetWork& RailNetWork::operator=(const RailNetWork& obj){
+    if (this == &obj){
+        return *this;
+    }
+    this->nodes = obj.nodes;
+    this->rails = obj.rails;
+    return *this;
+}
+
+
--- src/railNetWork.hpp
+++ src/railNetWork.hpp
@@ -2,6 +2,7 @@
 #define RAILNETWORK_HPP
 
 #include <iostream>
+#include <map>
 #include "node.hpp"
 #include "rail.hpp"
 
@@ -9,10 +10,16 @@
 class RailNetWork{
 
     private:
-
+        std::map<std::string, Node*>    nodes;
+        std::vector<Rail*>              rails;
 
     public:
+        RailNetWork():nodes(),rails(){}
+        RailNetWork(const RailNetWork& railNetwork);
+        RailNetWork& operator=(const RailNetWork& railNetWork);
+
         
+
     
 };
 
